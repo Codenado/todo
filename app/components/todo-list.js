fab6fa925b8d3f8c60e6423410937371f8a09582
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-06-12 14:34:49 -0700

    works

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -6,37 +6,44 @@ export default Ember.Component.extend({
 			this.set('newTitle', '');
 			this.sendAction('createTodo', newTitle)
 		},
-		remaining: Ember.computed('model.@each.isCompleted', function() {
-			var model = this.get('model');
-			return model.filterBy('isCompleted', false).get('length');
 
-		}),
-		infection: Ember.computed('remaining', function(){
-			var remaining =this.get('remaining');
-			return (remaining === 1 ) ? 'todo' : 'todos';
-		}),
 		clearCompleted: function() {
 			var complete = this.get('model').filterBy('isCompleted', true);
 			complete.invoke('deleteRecord')
 			complete.invoke('save')
 		},
+	},	
 		hasCompleted: Ember.computed('completed', function() {
     		return this.get('completed') > 0;
 		}),
+
 		completed: Ember.computed('model.@each.isCompleted', function() {
    			var model = this.get('model');
     		return model.filterBy('isCompleted', true).get('length');
 		}),
+		remaining: Ember.computed('model.@each.isCompleted', function() {
+			var model = this.get('model');
+			return model.filterBy('isCompleted', false).get('length');
+		}),
+
+		inflection: Ember.computed('remaining', function(){
+			var remaining =this.get('remaining');
+			return (remaining === 1 ) ? 'todo' : 'todos';
+
+		}),
+
 		allAreDone: function(key, value) {
-    		var model = this.get('model');
-    		console.log(key + ": " + value);
-    		if (value === undefined) {
-        		return model.get('length') > 0 && model.isEvery('isCompleted', true);
-    		} else {
-        		model.setEach('isCompleted', value);
-        		model.invoke('save');
-        		return value;
-			}
+		    var model = this.get('model');
+		    if (value === undefined) {
+		        return model.get('length') > 0 && model.isEvery('isCompleted', true);
+		    } else {
+		        model.setEach('isCompleted', value);
+		        model.invoke('save');
+		        return value;
+		    }
 		}.property('@each.isCompleted')
-	}
+
+
+		
+	
 });
